a2e306b00ca54705c76dc2077c6d9bb23a804c4f
Oywayten <oywayten@gmail.com>
2024-06-30 11:16:28 +0300

    Creating an authorization server

---
+++ src/main/java/doners/authorization/AuthServerApplication.java
@@ -0,0 +1,26 @@
+package doners.authorization;
+
+import doners.authorization.users.User;
+import doners.authorization.users.UserRepository;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@SpringBootApplication
+public class AuthServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthServerApplication.class, args);
+    }
+
+    @Bean
+    public ApplicationRunner dataLoader(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        return args -> {
+            userRepository.save(new User("habuma", passwordEncoder.encode("password"), "ROLE_ADMIN"));
+            userRepository.save(new User("donerchef", passwordEncoder.encode("password"), "ROLE_ADMIN"));
+        };
+    }
+
+}
---
+++ src/main/java/doners/authorization/users/User.java
@@ -0,0 +1,65 @@
+package doners.authorization.users;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.io.Serial;
+import java.util.Collection;
+import java.util.Collections;
+
+@Data
+@Entity
+@Table(name = "doner_user", uniqueConstraints = {@UniqueConstraint(columnNames = "username")})
+@RequiredArgsConstructor
+@NoArgsConstructor(force = true)
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class User implements UserDetails {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private final String username;
+    private final String password;
+    private final String role;
+
+    @Override
+    @Transient
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(new SimpleGrantedAuthority(role));
+    }
+
+    @Override
+    @Transient
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    @Transient
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    @Transient
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    @Transient
+    public boolean isEnabled() {
+        return true;
+    }
+
+}
